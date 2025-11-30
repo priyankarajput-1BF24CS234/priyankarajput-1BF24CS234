19cf2a1bd43573ab183de2956ac50b85c8e3bdb5
priyankarajput-1BF24CS234 <priyankarajput.cs24@bmsce.ac.in>
2025-11-30 22:35:04 +0530

    Update implementation.c

--- DS Lab/singly linked list/implementation.c
+++ DS Lab/singly linked list/implementation.c
@@ -3,31 +3,21 @@
 
 struct node {
     int data;
-    struct node* next;
+    struct node *next;
 };
 
 struct node *head = NULL;
 
-void createlist(int n) {
+void createlist() {
     struct node *newnode, *temp;
-    int data;
-
-    if (n <= 0) {
-        printf("Number of nodes should be greater than 0.\n");
-        return;
-    }
+    int n;
+    printf("\nEnter number of elements in the list: ");
+    scanf("%d", &n);
 
     for (int i = 1; i <= n; i++) {
-        newnode = (struct node*)malloc(sizeof(struct node));
-        if (newnode == NULL) {
-            printf("Memory not allocated.\n");
-            return;
-        }
-
-        printf("Enter data to be inserted: ");
-        scanf("%d", &data);
-
-        newnode->data = data;
+        newnode = (struct node *)malloc(sizeof(struct node));
+        printf("\nEnter data: ");
+        scanf("%d", &newnode->data);
         newnode->next = NULL;
 
         if (head == NULL) {
@@ -37,76 +27,115 @@ void createlist(int n) {
             temp = newnode;
         }
     }
+
+    printf("\nLinked list created successfully.\n");
 }
 
-void insertatbeg(int data) {
-    struct node *newnode;
-    newnode = (struct node*)malloc(sizeof(struct node));
-
-    if (newnode == NULL) {
-        printf("Memory not allocated.\n");
-        return;
-    }
-
-    newnode->data = data;
+void insertatbeg() {
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    printf("\nEnter data: ");
+    scanf("%d", &newnode->data);
     newnode->next = head;
     head = newnode;
+    printf("\nNode inserted at beginning.\n");
 }
 
-void insertatend(int data) {
-    struct node *newnode, *temp;
-    newnode = (struct node*)malloc(sizeof(struct node));
-
-    if (newnode == NULL) {
-        printf("Memory not allocated.\n");
-        return;
-    }
-
-    newnode->data = data;
+void insertatend() {
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    printf("\nEnter data: ");
+    scanf("%d", &newnode->data);
     newnode->next = NULL;
 
     if (head == NULL) {
         head = newnode;
     } else {
-        temp = head;
-        while (temp->next != NULL) {
+        struct node *temp = head;
+        while (temp->next != NULL)
             temp = temp->next;
-        }
         temp->next = newnode;
     }
+    printf("\nNode inserted at end.\n");
+}
+
+void insertatpos() {
+    int pos;
+    struct node *temp = head;
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+
+    printf("\nEnter data: ");
+    scanf("%d", &newnode->data);
+    printf("Enter position: ");
+    scanf("%d", &pos);
+
+    if (pos < 1) {
+        printf("Invalid position\n");
+        return;
+    } else if (pos == 1) {
+        newnode->next = head;
+        head = newnode;
+    } else {
+        for (int i = 1; i < pos - 1 && temp != NULL; i++)
+            temp = temp->next;
+
+        if (temp == NULL) {
+            printf("Invalid position\n");
+            free(newnode);
+            return;
+        }
+
+        newnode->next = temp->next;
+        temp->next = newnode;
+    }
+
+    printf("Node inserted at position %d\n", pos);
 }
 
 void display() {
-    struct node *temp = head;
     if (head == NULL) {
-        printf("List is empty.\n");
+        printf("\nList is empty.\n");
         return;
     }
 
-    printf("Linked list elements: ");
+    struct node *temp = head;
+    printf("\nLinked List: ");
     while (temp != NULL) {
-        printf("%d -> ", temp->data);
+        printf("%d ", temp->data);
         temp = temp->next;
     }
-    printf("NULL\n");
 }
 
 int main() {
-    int n;
+    int choice;
+    printf("Linked List Menu:\n");
+    printf("1. Create Linked List\n");
+    printf("2. Insert at Beginning\n");
+    printf("3. Insert at End\n");
+    printf("4. Insert at Position\n");
+    printf("5. Display\n");
+    printf("6. Exit\n");
 
-    printf("Enter number of nodes: ");
-    scanf("%d", &n);
-    createlist(n);
+    while (1) {
+        printf("\nEnter choice: ");
+        scanf("%d", &choice);
 
-    display();
-
-    insertatbeg(100);
-    printf("After inserting at beginning:\n");
-    display();
-
-    insertatend(999);
-    printf("After inserting at end:\n");
-    display();
+        switch (choice) {
+            case 1: createlist();
+            break;
+            case 2: insertatbeg();
+            break;
+            case 3: insertatend();
+            break;
+            case 4: insertatpos();
+            break;
+            case 5: display();
+            break;
+            case 6:
+                printf("Exiting program.\n");
+                return 0;
+            default:
+                printf("Invalid choice.\n");
+        }
+    }
 
     return 0;
-}
+
